44400991a05a83b60dc8594d0bf07efb066cfd1c
AntonNovozhilov <wowlass63@gmail.com>
2025-06-18 12:21:08 +0300

    fix review

--- app/api/routers.py
+++ app/api/routers.py
@@ -4,5 +4,9 @@ from .endpoints import dotaton, project, users
 
 main_router = APIRouter()
 main_router.include_router(users)
-main_router.include_router(project, tags=["charity_projects"], prefix='/charity_project')
-main_router.include_router(dotaton, tags=["donations"], prefix='/donation')
+main_router.include_router(
+    project,
+    tags=["charity_projects"],
+    prefix="/charity_project",
+)
+main_router.include_router(dotaton, tags=["donations"], prefix="/donation")
